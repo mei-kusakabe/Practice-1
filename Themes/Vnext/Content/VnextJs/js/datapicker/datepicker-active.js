1f374e9eceb787951fcc313fd8a45c25309e31ff
jafrin-iqbal-chowdhury <ishaishrat75@gmail.com>
2023-02-21 04:45:23 +0600

    first commit

---
+++ Themes/Vnext/Content/VnextJs/js/datapicker/datepicker-active.js
@@ -0,0 +1,124 @@
+(function ($) {
+    "use strict";
+
+    $('#data_1 .input-group.date').datepicker({
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true
+    });
+
+    $('#data_2 .input-group.date').datepicker({
+
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true
+    });
+
+    $('#Datewise-From .input-group.date').datepicker({
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true,
+        format: "dd/mm/yyyy"
+    });
+
+    $('#Datewise-To .input-group.date').datepicker({
+
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true,
+        format: "dd/mm/yyyy"
+    });
+
+    $('#DatewiseFrom').datepicker({
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true,
+        format: "dd/mm/yyyy"
+    });
+
+    $('#DatewiseTo').datepicker({
+
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true,
+        format: "dd/mm/yyyy"
+    });
+
+    $('#OrderPlaceFromDate').datepicker({
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true,
+        format: "dd/mm/yyyy"
+    });
+
+    $('#OrderPlaceToDate').datepicker({
+
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true,
+        format: "dd/mm/yyyy"
+    });
+
+    $('#FromDate').datepicker({
+
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true,
+        format: "dd/mm/yyyy"
+    });
+    $('#ToDate').datepicker({
+
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true,
+        format: "dd/mm/yyyy"
+    });
+
+    $('#ChequeDDDate').datepicker({
+
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true,
+        format: "dd/mm/yyyy"
+    });
+
+
+
+    $('#data_4 .input-group.date').datepicker({
+        minViewMode: 1,
+        keyboardNavigation: false,
+        forceParse: false,
+        forceParse: false,
+        autoclose: true,
+        todayHighlight: true
+    });
+
+    $('#data_5 .input-daterange').datepicker({
+        keyboardNavigation: false,
+        forceParse: false,
+        autoclose: true
+    });
+
+})(jQuery);
---
+++ Themes/Vnext/WebDesign/js/script.js
@@ -0,0 +1,9 @@
+/**************************
+
+Thx to Travis from @devtips
+
+you have to visit his youtube channel
+
+https://www.youtube.com/user/DevTipsForDesigners/videos
+
+****************************/
